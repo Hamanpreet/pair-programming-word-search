0ea4b8d14a730cc5f42a7e48234e8bb9f9b0b660
Hamanpreet <haman42626@gmail.com>
2023-05-21 21:26:50 -0400

    Make the code DRY require the transpose function

--- wordsearch.js
+++ wordsearch.js
@@ -1,17 +1,4 @@
-const transpose = function (matrix) {
-    let arr1 = [];
-    let arr = [];
-    let l1 = matrix[0].length;
-    let l2 = matrix.length
-    for (let i = 0; i < l1; i++) {
-      arr1 = [];
-      for (let y = 0; y < l2; y++) {
-        arr1.push(matrix[y][i]);
-      }
-      arr.push(arr1);
-    }
-    return arr;
-  };
+const transpose = require("../../../Week4/spoon.js");
 const wordSearch = (letters, word) => { 
     if (letters.length === 0 || !word) {
         return false;
